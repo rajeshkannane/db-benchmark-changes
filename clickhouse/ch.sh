03a813c1641c73276d0aa483f82c616a1521d30d
rajeshkannane <rajeshkannan.e@gmail.com>
2022-11-28 22:48:42 +0530

    ClickHouse Chanchanges made

--- clickhouse/ch.sh
+++ clickhouse/ch.sh
@@ -10,7 +10,7 @@ ch_stop() {
   sudo /usr/sbin/service clickhouse-server stop && sleep 10
 }
 ch_active() {
-  clickhouse-client --query="SELECT 0;" > /dev/null 2>&1
+  clickhouse-client --password 070590+Raju --query="SELECT 0;" > /dev/null 2>&1
   local ret=$?;
   if [[ $ret -eq 0 ]]; then return 0; elif [[ $ret -eq 210 ]]; then return 1; else echo "Unexpected return code from clickhouse-client: $ret" >&2 && return 1; fi;
 }
